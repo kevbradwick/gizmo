64e71f56d92239bc285e121af19f25454be0fcb3
Kevin Bradwick <kevinbradwick@gmail.com>
2016-12-29 22:18:44 +0000

    updated checkstyle info

---
+++ core/src/main/java/com/github/kevbradwick/gizmo/core/Config.java
@@ -0,0 +1,8 @@
+package com.github.kevbradwick.gizmo.core;
+
+public class Config {
+
+    public static Config defaultConfig() {
+        return new Config();
+    }
+}
--- core/src/main/java/com/github/kevbradwick/gizmo/core/Gizmo.java
+++ core/src/main/java/com/github/kevbradwick/gizmo/core/Gizmo.java
@@ -8,8 +8,29 @@ public class Gizmo {
 
     final private File destinationDirectory;
 
+    /**
+     * Create a new instance of Gizmo
+     *
+     * @param sourceDirectory the directory containing gherkin files
+     * @param destinationDirectory destination output directory
+     */
     public Gizmo(File sourceDirectory, File destinationDirectory) {
         this.sourceDirectory = sourceDirectory;
         this.destinationDirectory = destinationDirectory;
+    }
+
+    /**
+     * @param sourceDirectory absolute path to directory containing gherkin files
+     * @param destinationDirectory absolute path to the directory to output site to
+     */
+    public Gizmo(String sourceDirectory, String destinationDirectory) {
+        this(new File(sourceDirectory), new File(destinationDirectory));
+    }
+
+    /**
+     * Generate the html site from the source directory
+     */
+    public void process() {
+
     }
 }
